2c675ca7966f1168e7f23ff03c9cda4b7b4708d5
Karina Ferraz <ferrazgkarina@gmail.com>
2025-11-03 05:04:09 -0300

    create event form updated

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+
+  CATEGORİES = ["Arte", "Cultura", "Esporte", "Natureza", "Dança", "Gastronomia", "Política"]
+  CONFİRMATİON_DEADLİNES = ["No dia do evento", "1 dia antes", "3 dias antes", "1 semana antes"]
   belongs_to :user
 
   validates :title, :description, :category, :date, :time, presence: true
